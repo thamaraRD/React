0793ed102664555f715178dca15ebae58abde930
ThamaraRD <rdthata@gmail.com>
2022-10-20 12:34:36 -0300

    file components

--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { CounterApp } from "./CounterApp";
-import { FirstApp } from './FirstApp';
+import { CounterApp } from "../src/components/CounterApp";
+import { FirstApp } from '../src/components/FirstApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
