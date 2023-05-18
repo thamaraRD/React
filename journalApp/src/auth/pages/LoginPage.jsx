66d6bd9bf03daf5652f0a2e28f3a863d4e158497
thamara <rdthata@gmail.com>
2023-05-18 16:22:02 -0400

    probando material ui

---
+++ journalApp/src/auth/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+
+export const LoginPage = () => {
+  return (
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{minHeight: '100vh', background: 'primary.main', padding: 4}}
+    >
+
+    </Grid>
+  )
+}
